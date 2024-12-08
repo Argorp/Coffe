f7d6cd9255fd83732bc6732f2e792f940d785f8a
Progra <virus4buk@yandex.ru>
2024-12-08 11:40:16 +0800

    Добавление записи кофе

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 import sys
 import sqlite3
-from PyQt6.QtWidgets import QMainWindow, QTableWidgetItem, QApplication
+from PyQt6.QtWidgets import QMainWindow, QTableWidgetItem, QApplication, QWidget
 from PyQt6 import uic
 from collections import defaultdict
+from main2 import Ui_MainWindow
+from addEditCoffeeForm import Ui_Form
 
 
-class Main(QMainWindow):
+class Main(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.con = None
+        self.setupUi(self)
         self.roasting_translate = defaultdict(str)
         self.variation_translate = defaultdict(str)
-        self.setGeometry(0, 0, 600, 600)
-        uic.loadUi("main.ui", self)
+        self.setGeometry(0, 0, 800, 800)
+        # uic.loadUi("main2.ui", self)
         self.ui()
 
     def ui(self):
@@ -24,6 +27,7 @@ class Main(QMainWindow):
             self.roasting_translate[str(i[0])] = i[1]
         for i in view:
             self.variation_translate[str(i[0])] = i[1]
+        self.new_rep.clicked.connect(self.upd)
 
     def reset_table(self):
         header = ['Name', 'Degree of roasting', 'Variation', 'Description of taste',
@@ -42,6 +46,25 @@ class Main(QMainWindow):
                     cur = self.variation_translate[cur]
                 if j != 0:
                     self.table.setItem(i + 1, j - 1, QTableWidgetItem(cur))
+
+    def upd(self):
+        self.upd_table = Upd_Table(self)
+        self.upd_table.show()
+
+
+class Upd_Table(QWidget, Ui_Form):
+    def __init__(self, main: Main):
+        super().__init__()
+        self.cur_main = main
+        self.setupUi(self)
+        self.ui()
+
+    def ui(self):
+        self.pushButton.clicked.connect(self.ins_rep)
+
+    """Добавление записи в таблицу"""
+    def ins_rep(self):
+        self.lineEdit.setText("Пока не работает :)")
 
 
 if __name__ == "__main__":
